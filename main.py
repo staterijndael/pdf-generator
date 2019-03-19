ac42db412d882104e75bafffafba19b06789ee7d
oringik <zikalol2@mail.ru>
2019-03-19 18:27:00 +0500

    доделал функционал

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from bottle import run, route, request, response, template, Bottle
+from bottle import run, route, request, response, template, Bottle,error
 import json
 from fpdf import FPDF
 from PIL import Image
+import os.path
+import requests
+import json
 
 r = Bottle()
 
@@ -16,19 +19,33 @@ def resize_image(input_image_path,
 def convert_to_pdf(name):
     pdf = FPDF()
 
-    resize_image(input_image_path='images\\' + name + '.png',
-                 output_image_path='images\\' + name + '.png',
+    if(os.path.exists("images\\" + name)):
+
+        resize_image(input_image_path='images\\' + name,
+                 output_image_path='images\\' + name,
                  size=(620, 850))
 
-    image = "images\\" + name + ".png"
+        image = "images\\" + name
 
-    pdf.add_page()
-    pdf.image(image, 0, 0, 0, 0)
-    pdf.output("pdfs\\" + name + ".pdf", "F")
+        pdf.add_page()
+        pdf.image(image, 0, 0, 0, 0)
+        pdf.output("pdfs\\" + name + ".pdf", "F")
+
+        return True
+    else: return False
+
+
+
+@r.error(404)
+def error404(error):
+    return 'Ня, извините, страница не найдена!'
+
 
 @r.get('/x')
 @r.post('/x')
 def main_loop():
+    episodes = ""
+    item = False
     PythonDict = {}
     PythonDict['forms'] = {}
     for item in request.forms:
@@ -41,6 +58,21 @@ def main_loop():
     PythonDict['GET'] = {}
     for item in request.GET:
         PythonDict['GET'][item] = request.GET.get(item)
+        if item == "id":
+            r = requests.get("https://api.anibe.ru/posts/" + request.GET.get(item))
+            j = r.json()
+        elif item == "episode":
+            episodes = j["episodes"][request.GET.get(item)]
+            answer = ""
+            for v in episodes:
+                cuted_link = v.replace(":", "")
+                cuted_link = cuted_link.replace("/", "")
+                if (convert_to_pdf(cuted_link)):
+                    answer += "images\\" + v + ", "
+            if answer:
+                return json.dumps(PythonDict, indent=3) + "\n\n" + answer + "    had converted to PDF"
+            else:
+                return json.dumps(PythonDict, indent=3) + "\n"
 
     PythonDict['POST'] = {}
     for item in request.POST:
@@ -50,14 +82,7 @@ def main_loop():
     for item in request.params:
         PythonDict['params'][item] = request.params.get(item)
 
-
     # todo: Get the name of manga by ID ( request.GET.get(item) - ID of manga )
-
-    name = "3"   # name of got manga
-
-    convert_to_pdf(request.GET.get(item)) # WARNING!!!! PARAMETER CHANGE TO NAME OF THE MANGA. IT IS ID OF THE MANGA NOW
-
-    return json.dumps(PythonDict, indent=3) + "\n\n" + "images\\" + name + "    has converted to PDF"
 
 
 
